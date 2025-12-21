1c05b28e8667372db946516bfc83692958097a17
stromae <claudio.oliveira10.0p@gmail.com>
2025-12-21 19:16:56 -0300

    bug: falta de dependencias e options no teste

--- test/timeout/k6-redis.test.js
+++ test/timeout/k6-redis.test.js
@@ -1,3 +1,11 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 1,
+    iterations: 1,
+};
+
 export default function () {
     const res = http.get('http://127.0.0.1:3000/redis-slow');
 
